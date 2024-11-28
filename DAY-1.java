564817d57c39ae06de88df374f1e672341d25575
Atharva-Mulane <atharvamulane@gmail.com>
2024-11-28 21:30:28 +0530

    DAY-1

---
+++ DAY-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        int n = arr.length;
+        if (n < 2)
+            return -1;
+
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > first) {
+                second = first;
+                first = num;
+            } else if (num > second && num != first) {
+                second = num;
+            }
+        }
+
+        return (second == Integer.MIN_VALUE) ? -1 : second;
+    }
+}
